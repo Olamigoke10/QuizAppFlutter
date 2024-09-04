8763dcc36c2c66cd0a69ed9bf3525a9f64b7c475
MRBOX <olamigokeadebayo90@gmail.com>
2024-09-04 11:48:07 +0100

    Aligning the questions and the buttons

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -19,21 +19,26 @@ class _QuestionsScreenState extends State<QuestionsScreen>{
     // TODO: implement build
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-          currentQuestion.text, 
-          style: const TextStyle(
-            color: Colors.white,
-          ),
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((item) {
-            return AnswerButton(item, (){});
-          }),
-         
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+            currentQuestion.text, 
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map((item) {
+              return AnswerButton(item, (){});
+            }),
+           
+          ],
+        ),
       ),
     );
   }
