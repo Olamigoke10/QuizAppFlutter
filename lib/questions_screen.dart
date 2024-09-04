64de646338d98985d06e38abd2d5ce8f7c135c17
MRBOX <olamigokeadebayo90@gmail.com>
2024-09-04 11:40:00 +0100

    Using the map and the spread to add the answersbutton

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer.button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget{
   const QuestionsScreen({super.key});
@@ -14,21 +15,23 @@ class QuestionsScreen extends StatefulWidget{
 class _QuestionsScreenState extends State<QuestionsScreen>{
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
     // TODO: implement build
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Questions', 
-          style: TextStyle(
+          Text(
+          currentQuestion.text, 
+          style: const TextStyle(
             color: Colors.white,
           ),
           ),
-          SizedBox(height: 30),
-          AnswerButton('Answer1', () { }),
-          AnswerButton('Answer2', () { }),
-          AnswerButton('Answer3', () { }),
+          const SizedBox(height: 30),
+          ...currentQuestion.answers.map((item) {
+            return AnswerButton(item, (){});
+          }),
          
         ],
       ),
