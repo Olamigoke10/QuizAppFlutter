cae15185de0d27660d0184470d79e48a75db5d84
MRBOX <olamigokeadebayo90@gmail.com>
2024-09-04 10:21:18 +0100

    Configuring the column for the questions

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -14,6 +14,31 @@ class _QuestionsScreenState extends State<QuestionsScreen>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Text('Question Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Questions'),
+          SizedBox(height: 30),
+          ElevatedButton(
+          onPressed: () {},
+           child: Text('Answer 1')
+           ),
+          ElevatedButton(
+          onPressed: () {},
+           child: Text('Answer 2')
+           ),
+          ElevatedButton(
+          onPressed: () {},
+           child: Text('Answer 3')
+           ),
+          ElevatedButton(
+          onPressed: () {},
+           child: Text('Answer 4')
+           ),
+        ],
+      ),
+    );
   }
 }
